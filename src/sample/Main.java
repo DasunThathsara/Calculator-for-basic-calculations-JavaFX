eb64eeb611b71f92faeea37fc7a9cc6e363c2982
Dasun Thathsara <81586965+DasunThathsara@users.noreply.github.com>
2022-04-03 00:03:14 +0530

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        
-
         primaryStage.setTitle("Calc");
-
-
         primaryStage.setScene(new Scene(root, 250, 341));
         primaryStage.setResizable(false);
         primaryStage.show();
